64f53b1f7cc61158590ed13fed3d913be1d0eed4
Richard Jiang <richardejiang@hotmail.com>
2021-06-02 22:31:46 -0400

    Added json config file and enabled dynamic loading of json in stateless widget

--- lib/staggered_grid_view.dart
+++ lib/staggered_grid_view.dart
@@ -1,56 +1,78 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class StaggeredView extends StatelessWidget {
+
+  Future<Map> loadJson() async {
+    Map imageTexts = await rootBundle
+        .loadStructuredData('assets/config/staggered_text.json', (String s) async {
+      return json.decode(s);
+    });
+
+    return Future.value(imageTexts);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('testing grid view'),
-      ),
-      body: StaggeredGridView.count(
-        primary: false,
-        crossAxisCount: 4,
-        staggeredTiles: const <StaggeredTile>[
-          StaggeredTile.fit(2),
-          StaggeredTile.fit(2),
-          StaggeredTile.fit(1),
-          StaggeredTile.fit(3),
-          StaggeredTile.fit(3),
-          StaggeredTile.fit(1),
-          StaggeredTile.fit(2),
-          StaggeredTile.fit(2),
-        ],
-        children: const <Widget>[
-          _Tile(
-              //'https://cdn.pixabay.com/photo/2013/04/07/21/30/croissant-101636_960_720.jpg',
-              'steve-johnson.jpeg',
-              1),
-          _Tile(
-              //'https://cdn.pixabay.com/photo/2016/01/22/16/42/eiffel-tower-1156146_960_720.jpg',
-              'rodion-kutsaev.jpeg',
-              2),
-          _Tile(
-              //'https://cdn.pixabay.com/photo/2016/10/22/20/34/two-types-of-wine-1761613_960_720.jpg',
-              'efe-kurnaz.jpg',
-              3),
-          _Tile(
-              'https://cdn.pixabay.com/photo/2016/10/21/14/50/plouzane-1758197_960_720.jpg',
-              4),
-          _Tile(
-              'https://cdn.pixabay.com/photo/2016/11/16/10/59/mountains-1828596_960_720.jpg',
-              5),
-          _Tile(
-              'steve-johnson.jpeg',
-              6),
-          _Tile(
-              'https://cdn.pixabay.com/photo/2017/08/24/22/37/gyrfalcon-2678684_960_720.jpg',
-              7),
-          _Tile(
-              'https://cdn.pixabay.com/photo/2013/01/17/08/25/sunset-75159_960_720.jpg',
-              8),
-        ],
-      ),
+    return FutureBuilder(
+      future: loadJson(),
+      builder: (context, AsyncSnapshot<Map> snapshot) {
+        if (snapshot.hasData) {
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('testing grid view'),
+            ),
+            body: StaggeredGridView.count(
+              primary: false,
+              crossAxisCount: 4,
+              staggeredTiles: const <StaggeredTile>[
+                StaggeredTile.fit(1),
+                StaggeredTile.fit(2),
+                StaggeredTile.fit(1),
+                StaggeredTile.fit(2),
+                StaggeredTile.fit(1),
+                StaggeredTile.fit(2),
+                StaggeredTile.fit(2),
+                StaggeredTile.fit(2),
+              ],
+              children: <Widget>[
+                _Tile(
+                  //'https://cdn.pixabay.com/photo/2013/04/07/21/30/croissant-101636_960_720.jpg',
+                    snapshot.data!["1"],
+                    1),
+                _Tile(
+                  //'https://cdn.pixabay.com/photo/2016/01/22/16/42/eiffel-tower-1156146_960_720.jpg',
+                    'rodion-kutsaev.jpeg',
+                    2),
+                _Tile(
+                  //'https://cdn.pixabay.com/photo/2016/10/22/20/34/two-types-of-wine-1761613_960_720.jpg',
+                    'efe-kurnaz.jpg',
+                    3),
+                _Tile(
+                    'https://cdn.pixabay.com/photo/2016/10/21/14/50/plouzane-1758197_960_720.jpg',
+                    4),
+                _Tile(
+                    'https://cdn.pixabay.com/photo/2016/11/16/10/59/mountains-1828596_960_720.jpg',
+                    5),
+                _Tile(
+                    'steve-johnson.jpeg',
+                    6),
+                _Tile(
+                    'https://cdn.pixabay.com/photo/2017/08/24/22/37/gyrfalcon-2678684_960_720.jpg',
+                    7),
+                _Tile(
+                    'https://cdn.pixabay.com/photo/2013/01/17/08/25/sunset-75159_960_720.jpg',
+                    8),
+              ],
+            ),
+          );
+        } else {
+          return CircularProgressIndicator();
+        }
+      }
     );
   }
 }
