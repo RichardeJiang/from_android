b4697bdf94dde47acd300f6c6ec16bc5e2bcbbcc
Richard Jiang <richardejiang@hotmail.com>
2021-06-04 21:52:49 -0400

    Consolidated all image metadata into json and dynamically load it

--- lib/staggered_grid_view.dart
+++ lib/staggered_grid_view.dart
@@ -21,6 +21,7 @@ class StaggeredView extends StatelessWidget {
       future: loadJson(),
       builder: (context, AsyncSnapshot<Map> snapshot) {
         if (snapshot.hasData) {
+          final int numOfEntries = snapshot.data!.length;
           return Scaffold(
             appBar: AppBar(
               title: const Text('testing grid view'),
@@ -28,44 +29,16 @@ class StaggeredView extends StatelessWidget {
             body: StaggeredGridView.count(
               primary: false,
               crossAxisCount: 4,
-              staggeredTiles: const <StaggeredTile>[
-                StaggeredTile.fit(1),
-                StaggeredTile.fit(2),
-                StaggeredTile.fit(1),
-                StaggeredTile.fit(2),
-                StaggeredTile.fit(1),
-                StaggeredTile.fit(2),
-                StaggeredTile.fit(2),
-                StaggeredTile.fit(2),
+              staggeredTiles: <StaggeredTile>[
+                for (int i = 1; i <= numOfEntries; i++)
+                  StaggeredTile.fit(snapshot.data![i.toString()]["width"])
               ],
               children: <Widget>[
-                _Tile(
-                  //'https://cdn.pixabay.com/photo/2013/04/07/21/30/croissant-101636_960_720.jpg',
-                    snapshot.data!["1"],
-                    1),
-                _Tile(
-                  //'https://cdn.pixabay.com/photo/2016/01/22/16/42/eiffel-tower-1156146_960_720.jpg',
-                    'rodion-kutsaev.jpeg',
-                    2),
-                _Tile(
-                  //'https://cdn.pixabay.com/photo/2016/10/22/20/34/two-types-of-wine-1761613_960_720.jpg',
-                    'efe-kurnaz.jpg',
-                    3),
-                _Tile(
-                    'https://cdn.pixabay.com/photo/2016/10/21/14/50/plouzane-1758197_960_720.jpg',
-                    4),
-                _Tile(
-                    'https://cdn.pixabay.com/photo/2016/11/16/10/59/mountains-1828596_960_720.jpg',
-                    5),
-                _Tile(
-                    'steve-johnson.jpeg',
-                    6),
-                _Tile(
-                    'https://cdn.pixabay.com/photo/2017/08/24/22/37/gyrfalcon-2678684_960_720.jpg',
-                    7),
-                _Tile(
-                    'https://cdn.pixabay.com/photo/2013/01/17/08/25/sunset-75159_960_720.jpg',
-                    8),
+                for (int i = 1; i <= numOfEntries; i++)
+                  _Tile(
+                      snapshot.data![i.toString()]["source"],
+                      i,
+                      snapshot.data![i.toString()]["annotation"])
               ],
             ),
           );
@@ -78,10 +51,11 @@ class StaggeredView extends StatelessWidget {
 }
 
 class _Tile extends StatelessWidget {
-  const _Tile(this.source, this.index);
+  const _Tile(this.source, this.index, this.annotation);
 
   final String source;
   final int index;
+  final String annotation;
 
   @override
   Widget build(BuildContext context) {
@@ -94,7 +68,6 @@ class _Tile extends StatelessWidget {
     return Card(
       child: Column(
         children: <Widget>[
-          //Image.network(source),
           tileImage,
           Padding(
             padding: const EdgeInsets.all(4),
@@ -104,8 +77,8 @@ class _Tile extends StatelessWidget {
                   'Image number $index',
                   style: const TextStyle(fontWeight: FontWeight.bold),
                 ),
-                const Text(
-                  'Vincent Van Gogh',
+                Text(
+                  this.annotation,
                   style: TextStyle(color: Colors.grey),
                 ),
               ],
