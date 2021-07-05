a0672bd0809e57d1fc29ef3c92d9a0af727533eb
Richard Jiang <richardejiang@hotmail.com>
2021-07-04 20:29:45 -0400

    Changed mail view to stateful widget

--- lib/mail_view.dart
+++ lib/mail_view.dart
@@ -1,77 +1,67 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-import 'dart:async';
+class Mail extends StatefulWidget {
+  Mail();
 
-class Mail extends StatelessWidget {
+  @override
+  _MailState createState() => _MailState();
+}
 
-  Future<Map> loadJson() async {
-    Map imageTexts = await rootBundle
-        .loadStructuredData('assets/text/home_letter.json', (String s) async {
-      return json.decode(s);
-    });
+class _MailState extends State<Mail> {
+  String mailSender = '';
+  String mailContent = '';
 
-    return Future.value(imageTexts);
-  }
+  TextEditingController mailSenderController = TextEditingController();
+  TextEditingController mailContentController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: loadJson(),
-        builder: (context, AsyncSnapshot<Map> snapshot) {
-          if (snapshot.hasData) {
-            return Scaffold(
-              appBar: AppBar(
-                title: Text(
-                  '家书：',
-                  style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            '家书：',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          centerTitle: true,
+        ),
+        body: Column(
+            //mainAxisSize: MainAxisSize.max,
+            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width * 0.7,
+                margin: EdgeInsets.all(20),
+                child: TextField(
+                  controller: mailSenderController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Recipient',
+                  ),
                 ),
-                centerTitle: true,
               ),
-              body: Column(
-                    //mainAxisSize: MainAxisSize.max,
-                    // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    //crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        width: MediaQuery.of(context).size.width * 0.7,
-                        margin: EdgeInsets.all(20),
-                        child: TextField(
-                          controller: TextEditingController(text: 'The subject'),
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Recipient',
-                          ),
-                        ),
-                      ),
-                      Container(
-                        width: MediaQuery.of(context).size.width * 0.7,
-                        height: MediaQuery.of(context).size.height * 0.3,
-                        margin: EdgeInsets.all(20),
-                        child: TextField(
-                          controller: TextEditingController(text: 'The body'),
-                          maxLines: null,
-                          expands: true,
-                          textAlignVertical: TextAlignVertical.top,
-                          decoration: InputDecoration(
-                              labelText: 'Body', border: OutlineInputBorder()),
-                        ),
-                      ),
-
-                      Icon(
-                        Icons.send,
-                        color: Colors.black,
-                        size: 30,
-                      )
-                    ]
-                )
-              );
-          } else {
-            return CircularProgressIndicator();
-          }
-        }
+              Container(
+                width: MediaQuery.of(context).size.width * 0.7,
+                height: MediaQuery.of(context).size.height * 0.3,
+                margin: EdgeInsets.all(20),
+                child: TextField(
+                  controller: mailContentController,
+                  maxLines: null,
+                  expands: true,
+                  textAlignVertical: TextAlignVertical.top,
+                  decoration: InputDecoration(
+                      labelText: 'Body', border: OutlineInputBorder()),
+                ),
+              ),
+              Icon(
+                Icons.send,
+                color: Colors.black,
+                size: 30,
+              )
+            ]
+        )
     );
   }
 }
